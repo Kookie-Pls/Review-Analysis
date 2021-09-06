b78158b70a89fbac951f74588ba37bca714fff14
Alexander Estuesta <88150465+Kookie-Pls@users.noreply.github.com>
2021-09-06 11:27:28 +0100

    Add files via upload

---
+++ 6-agg-av-rat-day.py
@@ -0,0 +1,91 @@
+import justpy as jp
+from pandas.core.dtypes.common import classes
+import pandas
+from datetime import datetime
+from pytz import utc
+
+data = pandas.read_csv("reviews.csv", parse_dates=['Timestamp'])
+data['Weekday'] = data['Timestamp'].dt.strftime('%A')
+data['DayNumber'] = data['Timestamp'].dt.strftime('%w')
+
+weekday_average = data.groupby(['Weekday', 'DayNumber']).mean()
+weekday_average = weekday_average.sort_values('DayNumber')
+
+chart_def = """
+{
+    chart: {
+        type: 'spline',
+        inverted: false
+    },
+    title: {
+        text: 'Atmosphere Temperature by Altitude'
+    },
+    subtitle: {
+        text: 'According to the Course Reviews Dataset'
+    },
+    xAxis: {
+        reversed: false,
+        title: {
+            enabled: true,
+            text: 'Day'
+        },
+        labels: {
+            format: '{value}'
+        },
+        accessibility: {
+            rangeDescription: 'Range: 0 to 80 km.'
+        },
+        maxPadding: 0.05,
+        showLastLabel: true
+    },
+    yAxis: {
+        title: {
+            text: 'Rating'
+        },
+        labels: {
+            format: '{value}'
+        },
+        accessibility: {
+            rangeDescription: 'Range: -90°C to 20°C.'
+        },
+        lineWidth: 2
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br/>',
+        pointFormat: '{point.x} {point.y}'
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                enable: false
+            }
+        }
+    },
+    series: [{
+        name: 'Average Rating',
+        data: [[0, 15], [10, -50], [20, -56.5], [30, -46.5], [40, -22.1],
+            [50, -2.5], [60, -27.7], [70, -55.7], [80, -76.5]]
+    }]
+}
+"""
+
+
+def app():
+    wp = jp.QuasarPage()
+    h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews",
+                 classes="text-h3 text-center q-pa-md")
+    p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis")
+
+    hc = jp.HighCharts(a=wp, options=chart_def)
+    hc.options.title.text = "Aggregated Average Ratings by Day of the Week"
+    hc.options.xAxis.categories = list(
+        weekday_average.index.get_level_values(0))
+    hc.options.series[0].data = list(weekday_average['Rating'])
+
+    return wp
+
+
+jp.justpy(app)
